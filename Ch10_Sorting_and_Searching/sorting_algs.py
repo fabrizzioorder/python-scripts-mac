a94e1faa586740a7ec4de23e87943b823923af38
pforderique <porderiq@mit.edu>
2021-02-05 12:31:25 -0500

    CTCIs quick sort

--- Ch10_Sorting_and_Searching/sorting_algs.py
+++ Ch10_Sorting_and_Searching/sorting_algs.py
@@ -48,16 +48,16 @@ Memory: Depends
 
 Divide arr in half, sort both, and merge them
 '''
-# *CTCI's implementation
-def mergesort(arr):
+# *CTCI's implementation - memory: O(n) from helper
+def merge_sort(arr):
     helper = [0]*len(arr)
     merge_sort(arr, helper, 0, len(arr) - 1)
 
-def merge_sort(arr, helper, low, high):
+def mergesort(arr, helper, low, high):
     if low < high:
         mid = int(low + (high - low) / 2)
-        merge_sort(arr, helper, low, mid) # sort left half
-        merge_sort(arr, helper, mid + 1, high) # sort right half
+        mergesort(arr, helper, low, mid) # sort left half
+        mergesort(arr, helper, mid + 1, high) # sort right half
         merge(arr, helper, low, mid, high)
 
 def merge(arr, helper, low, mid, high):
@@ -87,9 +87,47 @@ def merge(arr, helper, low, mid, high):
     for idx in range(remaining + 1):
         arr[current + idx] = helper[helperLeft + idx]
 
+    # No need to copy in right half becuase its already THERE!
+
+'''
+QUICK SORT
+Runtime: O(nlogn) avg, O(n^2) worst case
+Memory: O(logn)
+
+Pick random element and partition the array such that all numbers that
+are less than the partitioning element come before all elements that 
+are greater than it. Keep partitioning until array is sorted
+'''
+# *CTCI's implementation
+def quick_sort(arr):
+    quicksort(arr, 0, len(arr) - 1)
+
+def quicksort(arr, left, right):
+    print(arr)
+    idx = partition(arr, left, right)
+
+    if left < idx - 1: # sort the left half
+        quicksort(arr, left, idx - 1)
+    if idx < right: # sort the right half
+        quicksort(arr, idx, right)
+
+def partition(arr, left, right):
+    pivot = arr[left + (right - left) // 2] # pick pivot position
+    while left <= right:
+        # find element on left that should be on right
+        while arr[left] < pivot: left += 1
+
+        # find element on right that should be on left
+        while arr[right] > pivot: right -= 1
+
+        # swap the elements and move left and right indices
+        if left <= right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+
+    return left
 
 ###############    TESTING    ################
 if __name__ == "__main__":
-    print(test2)
-    mergesort(test2)
-    print(test2)
+    quick_sort(test1)
