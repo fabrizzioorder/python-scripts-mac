d5c4828ccf249b13651b83ce0c256359f53debb1
pforderique <porderiq@mit.edu>
2021-02-05 12:07:31 -0500

    STCI Implementation of merge sort

--- Ch10_Sorting_and_Searching/sorting_algs.py
+++ Ch10_Sorting_and_Searching/sorting_algs.py
@@ -8,7 +8,7 @@ Most used:
 Merge Sort, Quick Sort, Radix Sort
 '''
 test1 = [10, 5, 4, 5, 2, 7, 1]
-test2 = [23,5,32,5,2,3,5,7,3,5,4,84,35,3]
+test2 = [23,5,32,5,2,3,5,7,3,5,4,35,3]
 
 '''
 Bubble Sort
@@ -16,7 +16,7 @@ Runtime: O(n^2) avg & worst case
 Memory: O(1)
 
 Start at beginning and swap the first two elements if the 1st is greater than 2nd
-THen go on to next pair and so on and repeat until all elements are sorted
+Then go on to next pair and so on and repeat until all elements are sorted
 '''
 def bubble_sort(arr) -> None:
     while True:
@@ -30,8 +30,66 @@ def bubble_sort(arr) -> None:
         if isSorted:
             break
 
+'''
+Selection Sort
+Runtime: O(n^2)
+Memory: O(1)
+
+Find smallest elem w/linear scan and swap with the first element.
+Repeat for rest of the list until all elems in place 
+'''
+def selection_sort(arr) -> None:
+    pass
+
+'''
+MERGE SORT
+Runtime: O(nlogn)
+Memory: Depends
+
+Divide arr in half, sort both, and merge them
+'''
+# *CTCI's implementation
+def mergesort(arr):
+    helper = [0]*len(arr)
+    merge_sort(arr, helper, 0, len(arr) - 1)
+
+def merge_sort(arr, helper, low, high):
+    if low < high:
+        mid = int(low + (high - low) / 2)
+        merge_sort(arr, helper, low, mid) # sort left half
+        merge_sort(arr, helper, mid + 1, high) # sort right half
+        merge(arr, helper, low, mid, high)
+
+def merge(arr, helper, low, mid, high):
+    # copy both halves into a helper array
+    for idx in range(low, high+1):
+        helper[idx] = arr[idx]
+
+    helperLeft = low
+    helperRight = mid + 1
+    current = low
+
+    # iterate through helper array. 
+    # Compare left and right halves copying back the smaller element 
+    # from the two halves into the original array
+    while(helperLeft <= mid and helperRight <= high):
+        if helper[helperLeft] <= helper[helperRight]:
+            arr[current] = helper[helperLeft]
+            helperLeft += 1
+        else: # if right element is smaller than left
+            arr[current] = helper[helperRight]
+            helperRight += 1
+
+        current += 1
+
+    # copy the rest of the left side of the array into the target arr
+    remaining = mid - helperLeft
+    for idx in range(remaining + 1):
+        arr[current + idx] = helper[helperLeft + idx]
+
+
 ###############    TESTING    ################
 if __name__ == "__main__":
-    print(test1)
-    bubble_sort(test1)
-    print(test1)
+    print(test2)
+    mergesort(test2)
+    print(test2)
